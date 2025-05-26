873d9517d0cdd3aa4d1cbc091045408bce5b2fed
Felipe Blaksley <felipe.blaksley@hotmail.com>
2025-05-26 12:36:52 -0300

    team grid

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -3,11 +3,55 @@
 	import heroImg from '../../assets/team-img.avif';
 	import DashedSeparator from '../../components/DashedSeparator.svelte';
 	import SkillBar from '../../components/SkillBar.svelte';
+	import TeamCard from '../../components/TeamCard.svelte';
 
 	const imageUrl = heroImg;
 	const title = 'Meet the Minds Behind Our Digital Success';
 	const description =
 		"We're a passionate collective of strategists, designers, and developers, united by a shared vision for digital innovation. Our diverse expertise and collaborative spirit drive us to create exceptional results for every client.";
+
+	const team = [
+		{
+			img: 'https://wgl-dsites.net/seofy/wp-content/uploads/2018/11/team_1-420x460.jpg',
+			name: 'Mark Liuw',
+			role: 'General Manager'
+		},
+		{
+			img: 'https://wgl-dsites.net/seofy/wp-content/uploads/2018/11/team_2-420x460.jpg',
+			name: 'Angela Kwang',
+			role: 'Chief Operating Officer'
+		},
+		{
+			img: 'https://wgl-dsites.net/seofy/wp-content/uploads/2018/11/team_3-420x460.jpg',
+			name: 'Nina Sherwood',
+			role: 'Senior Digital Strategist'
+		},
+		{
+			img: 'https://wgl-dsites.net/seofy/wp-content/uploads/2018/11/team_4-420x460.jpg',
+			name: 'Cherlyn Long',
+			role: 'Managing Director'
+		},
+		{
+			img: 'https://wgl-dsites.net/seofy/wp-content/uploads/2018/11/team_5-420x460.jpg',
+			name: 'Amy Cutter',
+			role: 'Head of Technical SEO'
+		},
+		{
+			img: 'https://wgl-dsites.net/seofy/wp-content/uploads/2018/11/team_6-420x460.jpg',
+			name: 'Mark Porter',
+			role: 'Head of Content'
+		},
+		{
+			img: 'https://wgl-dsites.net/seofy/wp-content/uploads/2018/11/team_7-420x460.jpg',
+			name: 'Matt Hopson',
+			role: 'Senior SEM Manager'
+		},
+		{
+			img: 'https://wgl-dsites.net/seofy/wp-content/uploads/2018/11/team_8-420x460.jpg',
+			name: 'Laura Brady',
+			role: 'SEO Consultant'
+		}
+	];
 </script>
 
 <div class="body">
@@ -51,6 +95,26 @@
 			</div>
 		</div>
 	</div>
+
+	<div
+		class="container"
+		style="flex-direction: column; align-items: center; justify-content: center;"
+	>
+		<div class="title-container">
+			<DashedSeparator />
+			<h1 class="black-title">Our Executive Team</h1>
+			<p class="black-text">
+				Our success is a result of teamwork and building upon our technical expertise and creative
+				style providing a full-service solution to our clients.
+			</p>
+		</div>
+
+		<div class="grid-container">
+			{#each team as team (team.name)}
+				<TeamCard img={team.img} name={team.name} role={team.role} />
+			{/each}
+		</div>
+	</div>
 </div>
 
 <style>
@@ -82,6 +146,9 @@
 	.img {
 		width: 100%;
 		height: 500px;
+		background-color: #f5f5f5;
+		padding: 20px;
+		border-radius: 20px;
 	}
 
 	.skillbar-container {
@@ -89,6 +156,35 @@
 		display: flex;
 		flex-direction: column;
 		gap: 25px;
+	}
+
+	.title-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		width: 700px;
+		text-align: center;
+		gap: 25px;
+	}
+
+	.grid-container {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr 1fr;
+		place-items: center;
+		gap: 25px;
+	}
+
+	@media (max-width: 1225px) {
+		.grid-container {
+			grid-template-columns: 1fr 1fr 1fr;
+		}
+	}
+
+	@media (max-width: 925px) {
+		.grid-container {
+			grid-template-columns: 1fr 1fr;
+		}
 	}
 
 	@media (max-width: 900px) {
@@ -103,9 +199,21 @@
 		}
 	}
 
+	@media (max-width: 750px) {
+		.title-container {
+			width: 100%;
+		}
+	}
+
 	@media (max-width: 700px) {
 		.sub-container {
 			width: 100%;
+		}
+	}
+
+	@media (max-width: 625px) {
+		.grid-container {
+			grid-template-columns: 1fr;
 		}
 	}
 
