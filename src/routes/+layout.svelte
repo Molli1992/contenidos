5643cec5533629e6a027f150d663b63351b0d1de
Felipe Blaksley <felipe.blaksley@hotmail.com>
2025-06-08 17:57:19 -0300

    cambio logo contenidos

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,7 +29,7 @@
 	<meta property="og:site_name" content="Contenidos" />
 	<meta
 		property="og:image"
-		content="https://wgl-dsites.net/unicoach/wp-content/uploads/2020/10/logo-B.png"
+		content="https://img.freepik.com/vector-premium/ilustracion-logotipo-letra-c-abstracta-estilo-moderno-minimalista_1375-6978.jpg"
 	/>
 	<meta property="og:image:width" content="1200" />
 	<meta property="og:image:height" content="630" />
@@ -46,7 +46,7 @@
 	/>
 	<meta
 		name="twitter:image"
-		content="https://wgl-dsites.net/unicoach/wp-content/uploads/2020/10/logo-B.png"
+		content="https://img.freepik.com/vector-premium/ilustracion-logotipo-letra-c-abstracta-estilo-moderno-minimalista_1375-6978.jpg"
 	/>
 </svelte:head>
 
