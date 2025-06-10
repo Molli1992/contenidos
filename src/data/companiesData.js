a0171b61c2ffd8e72cf7452028b6bd7112528a38
Felipe Blaksley <felipe.blaksley@hotmail.com>
2025-06-10 12:03:08 -0300

    imgs logos companies

--- src/data/companiesData.js
+++ src/data/companiesData.js
@@ -1,79 +1,87 @@
+import BACIUDAD from '../assets/images/logoCompanies/BA-ciudad.png';
+import COMCOM from '../assets/images/logoCompanies/Com-Com-helados.svg';
+import BECOOL from '../assets/images/logoCompanies/Becool-estética.png';
+import DECOWALL from '../assets/images/logoCompanies/Decowall.webp';
+import PARANASEGUROS from '../assets/images/logoCompanies/parana-seguros.webp';
+import DITOYS from '../assets/images/logoCompanies/Ditoys.png';
+import ENJOYPUNTADELESTE from '../assets/images/logoCompanies/Enjoy-Punta-del-Este.jpg';
+import LAFABRICA from '../assets/images/logoCompanies/la-fabrica.jpg';
+import DIBRA from '../assets/images/logoCompanies/Dibra.png';
+import FELFORT from '../assets/images/logoCompanies/Felfort.svg';
+import MONOBINGO from '../assets/images/logoCompanies/mono-bingo.png';
+import CEMEDYT from '../assets/images/logoCompanies/Cemedyt.jpeg';
+import LADORITA from '../assets/images/logoCompanies/La Dorita.png';
+import SAPHIRUS from '../assets/images/logoCompanies/Saphirus.webp';
+
 export const companiesData = [
 	{
 		id: 1,
-		imageUrl:
-			'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Logotipo_de_la_Ciudad_de_Buenos_Aires.svg/2560px-Logotipo_de_la_Ciudad_de_Buenos_Aires.svg.png',
+		imageUrl: BACIUDAD,
 		name: 'Buenos Aires Ciudad'
 	},
 	{
 		id: 2,
-		imageUrl: 'https://www.comcomhelados.com.ar/assets/img/logo-com-com.svg',
+		imageUrl: COMCOM,
 		name: 'Com-Com Helados'
 	},
 	{
 		id: 3,
-		imageUrl:
-			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTR0bkNk8EiDGFdTxlhniBI51tB_O8cfElp-w&s',
+		imageUrl: BECOOL,
 		name: 'Becool estética'
 	},
 	{
 		id: 4,
-		imageUrl: 'https://decowall.co.uk/cdn/shop/files/decoall-logo.png?v=1614318490',
+		imageUrl: DECOWALL,
 		name: 'Decowall'
 	},
 	{
 		id: 5,
-		imageUrl: 'https://majoseg.com.ar/wp-content/uploads/2023/09/parana-seguros.webp',
+		imageUrl: PARANASEGUROS,
 		name: 'Parana seguros'
 	},
 	{
 		id: 6,
-		imageUrl:
-			'https://pbs.twimg.com/profile_images/378800000019375231/72dc8a4ac319f3314a426f43b06d30a5_400x400.png',
+		imageUrl: DITOYS,
 		name: 'Ditoys'
 	},
 	{
 		id: 7,
-		imageUrl:
-			'https://enjoypuntadeleste.uy/wp-content/uploads/2023/06/logo-Enjoy-Punta-del-Este-vertical-gris.jpg',
+		imageUrl: ENJOYPUNTADELESTE,
 		name: 'Enjoy punta del este'
 	},
 	{
 		id: 8,
-		imageUrl: 'https://www.gaf-franquicias.com/images/franquicias/logo_franquicia_la_fabrica.jpg',
+		imageUrl: LAFABRICA,
 		name: 'La fabrica'
 	},
 	{
 		id: 9,
-		imageUrl:
-			'https://acdn-us.mitiendanube.com/stores/877/959/themes/common/logo-114092261-1578315368-0390a9c8390b02f2561e3bf5a4ff4f101578315368.png?0',
-		name: 'Dibra '
+		imageUrl: DIBRA,
+		name: 'Dibra'
 	},
 	{
 		id: 10,
-		imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Logo_de_Felfort.svg',
+		imageUrl: FELFORT,
 		name: 'Felfort '
 	},
 	{
 		id: 11,
-		imageUrl: 'https://img9.xooimage.com/files/2/0/b/monobing-1476c53.png',
+		imageUrl: MONOBINGO,
 		name: 'Mono bingo'
 	},
 	{
 		id: 12,
-		imageUrl:
-			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZjvUfo5cMopqQ2uGSwX7L9R9B7mylOFjraQ&s',
+		imageUrl: CEMEDYT,
 		name: 'Cemedyt'
 	},
 	{
 		id: 13,
-		imageUrl:
-			'https://images.squarespace-cdn.com/content/v1/666b5cdd0b9a0578fa8abca3/d37b9554-f088-4810-b653-2184b97ae4e1/parrilla-argentina-la-dorita-madrid.png',
+		imageUrl: LADORITA,
 		name: 'La Dorita'
 	},
 	{
 		id: 14,
-		imageUrl: 'https://aromatessen-saphirus.com.ar/wp-content/uploads/2023/12/logo-saphirus-.png',
+		imageUrl: SAPHIRUS,
 		name: 'Saphirus'
 	}
 ];
