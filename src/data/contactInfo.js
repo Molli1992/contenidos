7d1d568a2e349974f068368b2ff3f9c9c1bd3857
Felipe Blaksley <felipe.blaksley@hotmail.com>
2026-01-20 10:49:36 -0300

    cambio email

--- src/data/contactInfo.js
+++ src/data/contactInfo.js
@@ -1,6 +1,6 @@
 export const contactInfo = {
 	phone: '+54 9 11 5867-8101',
-	email: 'administracion@mediacontenidos.online',
+	email: 'administracion@ntgsa.com.ar',
 	whatsapp: '5491158678101',
 	youtube: '@danin95'
 };
